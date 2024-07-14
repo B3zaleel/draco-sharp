13400638cd226fdc3b1ea9b219a708648dba03e6
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-07-14 11:29:58 +0200

    Add Draco decoding example

--- src/Draco.Examples/Program.cs
+++ src/Draco.Examples/Program.cs
@@ -1,8 +1,17 @@
+using Draco.IO;
+
 namespace Draco.Examples;
 
 public static class Program
 {
     public static void Main()
     {
+        DecodeFile("Samples/house_04.obj.drc");
+    }
+
+    public static void DecodeFile(string path)
+    {
+        var decoder = new DracoDecoder();
+        decoder.Decode(path);
     }
 }
