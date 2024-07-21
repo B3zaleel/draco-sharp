66f9af9d467b44dcd21f3dc2ecfa34a6a490ace4
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-07-21 19:46:41 +0200

    Create the mesh traverser base

---
+++ src/Draco/IO/Mesh/Traverser/Traverser.cs
@@ -0,0 +1,50 @@
+using Draco.IO.Extensions;
+
+namespace Draco.IO.Mesh.Traverser;
+
+internal abstract class Traverser
+{
+    private readonly List<bool> _isFaceVisited = [];
+    private readonly List<bool> _isVertexVisited = [];
+    public CornerTable CornerTable { get; set; }
+    public TraversalObserver TraversalObserver { get; set; }
+
+    public Traverser(CornerTable cornerTable, TraversalObserver traversalObserver)
+    {
+        _isFaceVisited.Fill(cornerTable.FacesCount, false);
+        _isVertexVisited.Fill(cornerTable.VerticesCount, false);
+        CornerTable = cornerTable;
+        TraversalObserver = traversalObserver;
+    }
+
+    public bool IsFaceVisited(uint id, bool isFaceId)
+    {
+        if (isFaceId)
+        {
+            return id == Constants.kInvalidFaceIndex || _isFaceVisited[(int)id];
+        }
+        else
+        {
+            return id == Constants.kInvalidVertexIndex || _isFaceVisited[(int)id / 3];
+        }
+    }
+
+    public void MarkFaceVisited(uint faceId)
+    {
+        _isFaceVisited[(int)faceId] = true;
+    }
+
+    public bool IsVertexVisited(uint vertexId)
+    {
+        return _isVertexVisited[(int)vertexId];
+    }
+
+    public void MarkVertexVisited(uint vertexId)
+    {
+        _isVertexVisited[(int)vertexId] = true;
+    }
+
+    public virtual void Start() { }
+    public virtual void End() { }
+    public abstract void TraverseFromCorner(uint cornerId);
+}
