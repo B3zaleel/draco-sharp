86c73e87de25aa6c75b3669ff7a2fb28beecb13a
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-07-27 12:30:03 +0200

    Create a helper function for computing the square root of a generic number type

--- src/Draco/IO/Core/MathUtilities.cs
+++ src/Draco/IO/Core/MathUtilities.cs
@@ -2,6 +2,28 @@ namespace Draco.IO.Core;
 
 internal static class MathUtilities
 {
+    public static T IntSqrt<T>(T number)
+    {
+        if (number!.Equals(default(T)))
+        {
+            return default!;
+        }
+        var numberAsULong = (ulong)Convert.ChangeType(number, typeof(ulong));
+        ulong actNumber = numberAsULong;
+        ulong squareRoot = 1;
+
+        while (actNumber >= 2)
+        {
+            squareRoot *= 2;
+            actNumber /= 4;
+        }
+        do
+        {
+            squareRoot = (squareRoot + numberAsULong / squareRoot) / 2;
+        } while (squareRoot * squareRoot > numberAsULong);
+        return (T)Convert.ChangeType(squareRoot, typeof(T));
+    }
+
     public static T AddAsUnsigned<T>(T a, T b)
     {
         var unsignedResult = typeof(T).Name switch
