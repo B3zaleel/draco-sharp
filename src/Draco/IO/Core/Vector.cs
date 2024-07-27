42334d458006b7cb8f188aa3698403d94f30869a
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-07-27 13:05:54 +0200

    Implement a generic vector for storing scalar values and making vector-based computations

---
+++ src/Draco/IO/Core/Vector.cs
@@ -0,0 +1,251 @@
+using System.Numerics;
+
+namespace Draco.IO.Core;
+
+internal class Vector<TScalar>
+    where TScalar : struct,
+        IComparisonOperators<TScalar, TScalar, bool>,
+        IComparable,
+        IEqualityOperators<TScalar, TScalar, bool>,
+        IAdditionOperators<TScalar, TScalar, TScalar>,
+        ISubtractionOperators<TScalar, TScalar, TScalar>,
+        IDivisionOperators<TScalar, TScalar, TScalar>,
+        IMultiplyOperators<TScalar, TScalar, TScalar>,
+        IDecrementOperators<TScalar>,
+        IBitwiseOperators<TScalar, TScalar, TScalar>,
+        IMinMaxValue<TScalar>
+{
+    public byte Dimension { get; }
+    public TScalar[] Components { get; private set; }
+    public TScalar this[int index] { get => Components[index]; }
+
+    public Vector(byte dimension)
+    {
+        Dimension = dimension;
+        Components = new TScalar[dimension];
+
+        for (int i = 0; i < Dimension; ++i)
+        {
+            Components[i] = default;
+        }
+    }
+
+    public Vector(params TScalar[] components)
+    {
+        Dimension = (byte)components.Length;
+        Components = components;
+    }
+
+    public Vector(Vector<TScalar> otherVector)
+    {
+        Dimension = otherVector.Dimension;
+        Components = new TScalar[Dimension];
+        Array.Copy(otherVector.Components, Components, Dimension);
+    }
+
+    public static Vector<TScalar> operator *(Vector<TScalar> left, Vector<TScalar> right)
+    {
+        if (left.Dimension != right.Dimension)
+        {
+            throw new InvalidOperationException("Cannot add vectors of different dimensions.");
+        }
+        var result = new Vector<TScalar>(left.Dimension);
+
+        for (int i = 0; i < left.Dimension; ++i)
+        {
+            result.Components[i] = left.Components[i] * right.Components[i];
+        }
+        return result;
+    }
+
+    public static Vector<TScalar> operator +(Vector<TScalar> left, Vector<TScalar> right)
+    {
+        if (left.Dimension != right.Dimension)
+        {
+            throw new InvalidOperationException("Cannot add vectors of different dimensions.");
+        }
+        var result = new Vector<TScalar>(left.Dimension);
+
+        for (int i = 0; i < left.Dimension; ++i)
+        {
+            result.Components[i] = left.Components[i] + right.Components[i];
+        }
+        return result;
+    }
+
+    public static Vector<TScalar> operator -(Vector<TScalar> left, Vector<TScalar> right)
+    {
+        if (left.Dimension != right.Dimension)
+        {
+            throw new InvalidOperationException("Cannot subtract vectors of different dimensions.");
+        }
+        var result = new Vector<TScalar>(left.Dimension);
+
+        for (int i = 0; i < left.Dimension; ++i)
+        {
+            result.Components[i] = left.Components[i] - right.Components[i];
+        }
+        return result;
+    }
+
+    public static Vector<TScalar> operator *(Vector<TScalar> vector, TScalar scalar)
+    {
+        var result = new Vector<TScalar>(vector.Dimension);
+
+        for (int i = 0; i < vector.Dimension; ++i)
+        {
+            result.Components[i] = vector.Components[i] * scalar;
+        }
+        return result;
+    }
+
+    public static Vector<TScalar> operator *(TScalar scalar, Vector<TScalar> vector)
+    {
+        var result = new Vector<TScalar>(vector.Dimension);
+
+        for (int i = 0; i < vector.Dimension; ++i)
+        {
+            result.Components[i] = vector.Components[i] * scalar;
+        }
+        return result;
+    }
+
+    public static Vector<TScalar> operator /(Vector<TScalar> vector, TScalar scalar)
+    {
+        var result = new Vector<TScalar>(vector.Dimension);
+
+        for (int i = 0; i < vector.Dimension; ++i)
+        {
+            result.Components[i] = vector.Components[i] / scalar;
+        }
+        return result;
+    }
+
+    public static Vector<TScalar> operator /(TScalar scalar, Vector<TScalar> vector)
+    {
+        var result = new Vector<TScalar>(vector.Dimension);
+
+        for (int i = 0; i < vector.Dimension; ++i)
+        {
+            result.Components[i] = vector.Components[i] / scalar;
+        }
+        return result;
+    }
+
+    public static Vector<TScalar> operator +(Vector<TScalar> vector, TScalar scalar)
+    {
+        var result = new Vector<TScalar>(vector.Dimension);
+
+        for (int i = 0; i < vector.Dimension; ++i)
+        {
+            result.Components[i] = vector.Components[i] + scalar;
+        }
+        return result;
+    }
+
+    public static Vector<TScalar> operator +(TScalar scalar, Vector<TScalar> vector)
+    {
+        var result = new Vector<TScalar>(vector.Dimension);
+
+        for (int i = 0; i < vector.Dimension; ++i)
+        {
+            result.Components[i] = vector.Components[i] + scalar;
+        }
+        return result;
+    }
+
+    public static Vector<TScalar> operator -(Vector<TScalar> vector, TScalar scalar)
+    {
+        var result = new Vector<TScalar>(vector.Dimension);
+
+        for (int i = 0; i < vector.Dimension; ++i)
+        {
+            result.Components[i] = vector.Components[i] - scalar;
+        }
+        return result;
+    }
+
+    public static Vector<TScalar> operator -(TScalar scalar, Vector<TScalar> vector)
+    {
+        var result = new Vector<TScalar>(vector.Dimension);
+
+        for (int i = 0; i < vector.Dimension; ++i)
+        {
+            result.Components[i] = vector.Components[i] - scalar;
+        }
+        return result;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Vector<TScalar> vector)
+        {
+            if (vector.Dimension != Dimension)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < Dimension; ++i)
+            {
+                if (!vector.Components[i].Equals(Components[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public TScalar SquaredNorm()
+    {
+        return Dot(this);
+    }
+
+    public TScalar Dot(Vector<TScalar> otherVector)
+    {
+        if (Dimension != otherVector.Dimension)
+        {
+            throw new InvalidOperationException("Cannot calculate dot product of vectors of different dimensions.");
+        }
+        TScalar result = default;
+
+        for (int i = 0; i < Dimension; ++i)
+        {
+            result += Components[i] * otherVector.Components[i];
+        }
+        return result;
+    }
+}
+
+internal class Vector2<TScalar> : Vector<TScalar>
+    where TScalar : struct,
+        IComparisonOperators<TScalar, TScalar, bool>,
+        IComparable,
+        IEqualityOperators<TScalar, TScalar, bool>,
+        IAdditionOperators<TScalar, TScalar, TScalar>,
+        ISubtractionOperators<TScalar, TScalar, TScalar>,
+        IDivisionOperators<TScalar, TScalar, TScalar>,
+        IMultiplyOperators<TScalar, TScalar, TScalar>,
+        IDecrementOperators<TScalar>,
+        IBitwiseOperators<TScalar, TScalar, TScalar>,
+        IMinMaxValue<TScalar>
+{
+    public Vector2(TScalar c0, TScalar c1) : base(c0, c1) { }
+}
+
+internal class Vector3<TScalar> : Vector<TScalar>
+    where TScalar : struct,
+        IComparisonOperators<TScalar, TScalar, bool>,
+        IComparable,
+        IEqualityOperators<TScalar, TScalar, bool>,
+        IAdditionOperators<TScalar, TScalar, TScalar>,
+        ISubtractionOperators<TScalar, TScalar, TScalar>,
+        IDivisionOperators<TScalar, TScalar, TScalar>,
+        IMultiplyOperators<TScalar, TScalar, TScalar>,
+        IDecrementOperators<TScalar>,
+        IBitwiseOperators<TScalar, TScalar, TScalar>,
+        IMinMaxValue<TScalar>
+{
+    public Vector3(TScalar c0, TScalar c1, TScalar c2) : base(c0, c1, c2) { }
+}
