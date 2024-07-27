e925634f51ada63bcf51e9d34f7b45f666851c30
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-07-27 12:40:55 +0200

    Create the attribute transform base class

---
+++ src/Draco/IO/Attributes/AttributeTransform.cs
@@ -0,0 +1,32 @@
+namespace Draco.IO.Attributes;
+
+internal abstract class AttributeTransform
+{
+    public abstract AttributeTransformType Type { get; }
+
+    public void TransferToAttribute(PointAttribute attribute)
+    {
+        var transformData = new AttributeTransformData();
+        CopyToAttributeTransformData(transformData);
+        attribute.AttributeTransformData = transformData;
+    }
+
+    public PointAttribute InitTransformedAttribute(PointAttribute srcAttribute, int numEntries)
+    {
+        var numComponents = GetTransformedNumComponents(srcAttribute);
+        var dataType = GetTransformedDataType(srcAttribute);
+        var geometryAttribute = new GeometryAttribute(srcAttribute.AttributeType, null, (byte)numComponents, dataType, false, numComponents * Constants.DataTypeLength(dataType), 0);
+        var transformedAttribute = new PointAttribute(geometryAttribute);
+        transformedAttribute.Reset(numEntries);
+        transformedAttribute.SetIdentityMapping();
+        transformedAttribute.UniqueId = srcAttribute.UniqueId;
+        return transformedAttribute;
+    }
+
+    public abstract void CopyToAttributeTransformData(AttributeTransformData data);
+    public abstract void TransformAttribute(PointAttribute attribute, List<uint> pointIds, PointAttribute targetAttribute);
+    public abstract void InverseTransformAttribute(PointAttribute attribute, PointAttribute targetAttribute);
+    public abstract void DecodeParameters(DecoderBuffer decoderBuffer, PointAttribute targetAttribute);
+    public abstract DataType GetTransformedDataType(PointAttribute attribute);
+    public abstract int GetTransformedNumComponents(PointAttribute attribute);
+}
