86255f39706bc3de2740fd0da0e59756342b0bea
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-07-27 12:40:17 +0200

    Implement data retrieval, wriing, and appending in the attribute transform data

--- src/Draco/IO/Attributes/AttributeTransformData.cs
+++ src/Draco/IO/Attributes/AttributeTransformData.cs
@@ -1,6 +1,26 @@
+using Draco.IO.Extensions;
+
 namespace Draco.IO.Attributes;
 
 public class AttributeTransformData
 {
     public AttributeTransformType TransformType { get; set; } = AttributeTransformType.InvalidTransform;
+    public Stream? Buffer { get; set; }
+
+    public TDataType GetParameterValue<TDataType>(int byteOffset)
+    {
+        Buffer!.Seek(byteOffset, SeekOrigin.Begin);
+        return Buffer.ReadDatum<TDataType>();
+    }
+
+    public void SetParameterValue<TDataType>(int byteOffset, TDataType data)
+    {
+        Buffer!.Seek(byteOffset, SeekOrigin.Begin);
+        Buffer.WriteData([data]);
+    }
+
+    public void AppendParameterValue<TDataType>(TDataType data)
+    {
+        SetParameterValue((int)Buffer!.Length, data);
+    }
 }
