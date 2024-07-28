c9edb4dcffb65d52b40314fbba381754067d451f
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-07-28 09:05:29 +0200

    Implement routines for retrieving and converting  data from a geometry attribute's buffer

--- src/Draco/IO/Attributes/GeometryAttribute.cs
+++ src/Draco/IO/Attributes/GeometryAttribute.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Draco.IO.Extensions;
 
 namespace Draco.IO.Attributes;
@@ -52,5 +53,141 @@ public class GeometryAttribute
         Buffer = buffer;
         ByteStride = byteStride;
         ByteOffset = byteOffset;
+    }
+
+    public T GetValue<T>()
+    {
+        return Buffer!.ReadDatum<T>();
+    }
+
+    public T[] GetValue<T>(uint attributeId, int numComponents)
+    {
+        Buffer!.Seek(ByteOffset + ByteStride * attributeId, SeekOrigin.Begin);
+        return Buffer!.ReadData<T>(numComponents);
+    }
+
+    public TOut[] ConvertValue<TOut>(uint attributeId)
+        where TOut : struct,
+            IComparisonOperators<TOut, TOut, bool>,
+            IComparable,
+            IEqualityOperators<TOut, TOut, bool>,
+            IAdditionOperators<TOut, TOut, TOut>,
+            ISubtractionOperators<TOut, TOut, TOut>,
+            IDivisionOperators<TOut, TOut, TOut>,
+            IMultiplyOperators<TOut, TOut, TOut>,
+            IDecrementOperators<TOut>,
+            IBitwiseOperators<TOut, TOut, TOut>,
+            IMinMaxValue<TOut>
+    {
+        return ConvertValue<TOut>(attributeId, NumComponents);
+    }
+
+    public TOut[] ConvertValue<TOut>(uint attributeId, int numComponents)
+        where TOut : struct,
+            IComparisonOperators<TOut, TOut, bool>,
+            IComparable,
+            IEqualityOperators<TOut, TOut, bool>,
+            IAdditionOperators<TOut, TOut, TOut>,
+            ISubtractionOperators<TOut, TOut, TOut>,
+            IDivisionOperators<TOut, TOut, TOut>,
+            IMultiplyOperators<TOut, TOut, TOut>,
+            IDecrementOperators<TOut>,
+            IBitwiseOperators<TOut, TOut, TOut>,
+            IMinMaxValue<TOut>
+    {
+        return DataType switch
+        {
+            DataType.Int8 => ConvertTypedValue<sbyte, TOut>(attributeId, numComponents),
+            DataType.UInt8 => ConvertTypedValue<byte, TOut>(attributeId, numComponents),
+            DataType.Int16 => ConvertTypedValue<short, TOut>(attributeId, numComponents),
+            DataType.UInt16 => ConvertTypedValue<ushort, TOut>(attributeId, numComponents),
+            DataType.Int32 => ConvertTypedValue<int, TOut>(attributeId, numComponents),
+            DataType.UInt32 => ConvertTypedValue<uint, TOut>(attributeId, numComponents),
+            DataType.Int64 => ConvertTypedValue<long, TOut>(attributeId, numComponents),
+            DataType.UInt64 => ConvertTypedValue<ulong, TOut>(attributeId, numComponents),
+            DataType.Float32 => ConvertTypedValue<float, TOut>(attributeId, numComponents),
+            DataType.Float64 => ConvertTypedValue<double, TOut>(attributeId, numComponents),
+            // DataType.Bool => ConvertTypedValue<bool, TOut>(attributeId, numComponents),
+            _ => throw new InvalidOperationException("Invalid data type.")
+        };
+    }
+
+    public TOut[] ConvertTypedValue<T, TOut>(uint attributeId, int numComponents)
+        where T : struct,
+            IComparisonOperators<T, T, bool>,
+            IComparable,
+            IEqualityOperators<T, T, bool>,
+            IAdditionOperators<T, T, T>,
+            ISubtractionOperators<T, T, T>,
+            IDivisionOperators<T, T, T>,
+            IMultiplyOperators<T, T, T>,
+            IDecrementOperators<T>,
+            IBitwiseOperators<T, T, T>,
+            IMinMaxValue<T>
+        where TOut : struct,
+            IComparisonOperators<TOut, TOut, bool>,
+            IComparable,
+            IEqualityOperators<TOut, TOut, bool>,
+            IAdditionOperators<TOut, TOut, TOut>,
+            ISubtractionOperators<TOut, TOut, TOut>,
+            IDivisionOperators<TOut, TOut, TOut>,
+            IMultiplyOperators<TOut, TOut, TOut>,
+            IDecrementOperators<TOut>,
+            IBitwiseOperators<TOut, TOut, TOut>,
+            IMinMaxValue<TOut>
+    {
+        var size = Math.Min(numComponents, NumComponents);
+        var result = new TOut[size];
+
+        for (int i = 0; i < size; ++i)
+        {
+            var value = Buffer!.ReadDatum<T>();
+            result[i] = ConvertComponentValue<T, TOut>(value, Normalized);
+        }
+        return result;
+    }
+
+    private static TOut ConvertComponentValue<T, TOut>(T value, bool normalized)
+        where T : struct,
+            IComparisonOperators<T, T, bool>,
+            IComparable,
+            IEqualityOperators<T, T, bool>,
+            IAdditionOperators<T, T, T>,
+            ISubtractionOperators<T, T, T>,
+            IDivisionOperators<T, T, T>,
+            IMultiplyOperators<T, T, T>,
+            IDecrementOperators<T>,
+            IBitwiseOperators<T, T, T>,
+            IMinMaxValue<T>
+        where TOut : struct,
+            IComparisonOperators<TOut, TOut, bool>,
+            IComparable,
+            IEqualityOperators<TOut, TOut, bool>,
+            IAdditionOperators<TOut, TOut, TOut>,
+            ISubtractionOperators<TOut, TOut, TOut>,
+            IDivisionOperators<TOut, TOut, TOut>,
+            IMultiplyOperators<TOut, TOut, TOut>,
+            IDecrementOperators<TOut>,
+            IBitwiseOperators<TOut, TOut, TOut>,
+            IMinMaxValue<TOut>
+    {
+        if (Constants.IsIntegral<T>() && Constants.IsFloatingPoint<TOut>() && normalized)
+        {
+            var result = (TOut)Convert.ChangeType(value, typeof(TOut))!;
+            result /= (TOut)Convert.ChangeType(T.MaxValue, typeof(TOut))!;
+            return result;
+        }
+        else if (Constants.IsFloatingPoint<T>() && Constants.IsIntegral<TOut>() && normalized)
+        {
+            Assertions.ThrowIf(value < (T)Convert.ChangeType(0, typeof(T))! || value > (T)Convert.ChangeType(1, typeof(T))!);
+            Assertions.ThrowIf(Constants.SizeOf<T>() > 4);
+            var result = (TOut)Convert.ChangeType(Math.Floor((double)Convert.ChangeType(value, typeof(double))! * (double)Convert.ChangeType(TOut.MaxValue, typeof(double))! + 0.5), typeof(TOut))!;
+            return result;
+        }
+        else
+        {
+            var result = (TOut)Convert.ChangeType(value, typeof(TOut))!;
+            return result;
+        }
     }
 }
