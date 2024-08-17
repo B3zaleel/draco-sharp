6d8ba3d41bdd17d339a69de1b5bfb9e560d5c808
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-08-17 18:39:21 +0200

    Implement header encoding

---
+++ src/Draco/IO/DracoEncoder.cs
@@ -0,0 +1,32 @@
+namespace Draco.IO;
+
+public class DracoEncoder
+{
+    public void Encode(string path, Draco draco)
+    {
+        using var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+        Encode(new BinaryWriter(fs), draco);
+    }
+
+    public void Encode(Stream stream, Draco draco)
+    {
+        Encode(new BinaryWriter(stream), draco);
+    }
+
+    public void Encode(BinaryWriter binaryWriter, Draco draco)
+    {
+        using var encoderBuffer = new EncoderBuffer(binaryWriter);
+        EncodeHeader(encoderBuffer, draco.Header);
+        encoderBuffer.BitStreamVersion = draco.Header.Version;
+    }
+
+    private static void EncodeHeader(EncoderBuffer encoderBuffer, DracoHeader header)
+    {
+        encoderBuffer.WriteASCIIBytes(Constants.DracoMagic);
+        encoderBuffer.WriteByte(header.MajorVersion);
+        encoderBuffer.WriteByte(header.MinorVersion);
+        encoderBuffer.WriteByte(header.EncoderType);
+        encoderBuffer.WriteByte(header.EncoderMethod);
+        encoderBuffer.WriteUInt16(header.Flags);
+    }
+}
