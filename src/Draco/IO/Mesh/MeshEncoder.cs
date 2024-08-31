9f5ef09f669199b023cf8547ac0fbe9dc5b4db46
Bezaleel Olakunori <bezaleeloci@gmail.com>
2024-08-31 16:16:08 +0200

    Create the base class for mesh encoder

---
+++ src/Draco/IO/Mesh/MeshEncoder.cs
@@ -0,0 +1,26 @@
+using Draco.IO.Attributes;
+
+namespace Draco.IO.Mesh;
+
+/// <summary>
+/// Represents a class that constructs a 3D mesh data to input data that can be decoded by a <see cref="MeshDecoder"/>.
+/// </summary>
+internal abstract class MeshEncoder(Config config) : ConnectivityEncoder(config)
+{
+    public Mesh Mesh { get; protected set; } = new();
+    public override PointCloud.PointCloud? PointCloud { get => Mesh; }
+    public virtual CornerTable? CornerTable { get; protected set; }
+    public int EncodedFacesCount { get; protected set; }
+
+    public virtual MeshAttributeCornerTable? GetAttributeCornerTable(int attId)
+    {
+        return null;
+    }
+
+    public virtual MeshAttributeIndicesEncodingData? GetAttributeEncodingData(int attId)
+    {
+        return null;
+    }
+
+    public abstract void ComputeNumberOfEncodedFaces();
+}
